1b398e8d1f353cab78998f3c82d12cdbb8e9db64
stevie2codes <stevie2codes@gmail.com>
2019-12-16 17:35:29 -0600

    Changed scroll animation and added scroll up button

--- script.js
+++ script.js
@@ -3,21 +3,41 @@ $(document).ready(function(){
    let controller = new ScrollMagic.Controller();
 
    new ScrollMagic.Scene({
-       triggerElement: '#experience', 
+       triggerElement: '#experience', offset: -300
    })
        .setClassToggle('#experience', 'fade-in')
        .addTo(controller);
 
        new ScrollMagic.Scene({
-         triggerElement: '#portfolio', 
+         triggerElement: '#portfolio', offset: -300
      })
          .setClassToggle('#portfolio', 'fade-in')
          .addTo(controller);
 
          new ScrollMagic.Scene({
-            triggerElement: '#contact', 
+            triggerElement: '#contact', offset: -300
         })
             .setClassToggle('#contact', 'fade-in')
             .addTo(controller);
-});
+ 
 
+
+
+});
+let myButton = document.getElementById('myBtn');
+
+window.onscroll = function() {scrollFunction()};
+
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+            myButton.style.display = "block";
+        } else {
+            myButton.style.display = "none";
+        }
+    }
+
+        function topFunction(){
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
