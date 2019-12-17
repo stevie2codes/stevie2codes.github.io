ee9b1d5818b4185c2ba53e436c35ccc56a1e2488
stevie2codes <stevie2codes@gmail.com>
2019-12-17 15:54:55 -0600

    adjusted background element animation

--- script.js
+++ script.js
@@ -3,39 +3,39 @@ $(document).ready(function () {
     let controller = new ScrollMagic.Controller();
 
     new ScrollMagic.Scene({
-        triggerElement: '#experience', 
+        triggerElement: '#experience',
     })
         .setTween("#experience", 0.5, { left: 0 })
         .addTo(controller);
 
-       new ScrollMagic.Scene({
-         triggerElement: '#portfolio'
-     })
-     .setTween("#portfolio", 0.5, { left: 0 })
-     .addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: '#portfolio'
+    })
+        .setTween("#portfolio", 0.5, { left: 0 })
+        .addTo(controller);
 
-         new ScrollMagic.Scene({
-            triggerElement: '#contact', offset: -300
-        })
-            .setClassToggle('#contact', 'fade-in')
-            .addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: '#contact', offset: -300
+    })
+        .setClassToggle('#contact', 'fade-in')
+        .addTo(controller);
 
-        });
+});
 
-    let myButton = document.getElementById('myBtn');
+let myButton = document.getElementById('myBtn');
 
-    window.onscroll = function () { scrollFunction() };
+window.onscroll = function () { scrollFunction() };
 
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            myButton.style.display = "block";
-        } else {
-            myButton.style.display = "none";
-        }
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
     }
+}
 
-    function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
