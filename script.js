fc25e77aacd3bc1f01c5d1d52750959d1dc4aac0
stevie2codes <stevie2codes@gmail.com>
2019-12-17 11:22:38 -0600

    testing gsap

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-   let controller = new ScrollMagic.Controller();
+    let controller = new ScrollMagic.Controller();
 
-   new ScrollMagic.Scene({
-       triggerElement: '#experience', offset: -300
-   })
-       .setClassToggle('#experience', 'fade-in')
-       .addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: '#experience', 
+    })
+        .setTween("#experience", 0.5, { left: 0 })
+        .addTo(controller);
 
        new ScrollMagic.Scene({
          triggerElement: '#portfolio', offset: -300
@@ -19,25 +19,28 @@ $(document).ready(function(){
         })
             .setClassToggle('#contact', 'fade-in')
             .addTo(controller);
- 
 
 
 
-});
-let myButton = document.getElementById('myBtn');
 
-window.onscroll = function() {scrollFunction()};
+
+
+
+    let myButton = document.getElementById('myBtn');
+
+    window.onscroll = function () { scrollFunction() };
 
 
     function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             myButton.style.display = "block";
         } else {
             myButton.style.display = "none";
         }
     }
 
-        function topFunction(){
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+});
